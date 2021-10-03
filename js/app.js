63ad92fd0bbd269e69825924f4b0010f7039d781
Erich Hartnauer <erich.hartnauer@gmail.com>
2021-10-03 17:45:51 -0500

    completed extended about-me lab

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 'use strict';
 
+let score = 0;
 let userName = prompt('Hello, what is your name?');
 
 alert(`Hello ${userName}! Welcome to my About Me page!`);
@@ -13,6 +14,7 @@ let questionOne = prompt(
 if (questionOne === 'YES' || questionOne === 'Y') {
   //console.log('You are correct!');
   alert('You are correct!');
+  score++;
 } else {
   //console.log('Sorry, that is incorrect. My favorite color is blue.');
   alert('Sorry, that is incorrect. My favorite color is blue.');
@@ -23,6 +25,7 @@ let questionTwo = prompt('Did I grow up in Texas?').toUpperCase();
 if (questionTwo === 'NO' || questionTwo === 'N') {
   //console.log('You are correct! I grew up in Oklahoma.');
   alert('You are correct! I grew up in Oklahoma.');
+  score++;
 } else {
   //console.log('Sorry, that is incorrect. I grew up in Oklahoma');
   alert('Sorry, that is incorrect. I grew up in Oklahoma');
@@ -33,6 +36,7 @@ let questionThree = prompt('Is my favorite meal lasagna?').toUpperCase();
 if (questionThree === 'YES' || questionThree === 'Y') {
   //console.log('You are correct!');
   alert('You are correct!');
+  score++;
 } else {
   //console.log('Sorry, that is incorrect.');
   alert('Sorry, that is incorrect.');
@@ -46,6 +50,7 @@ if (questionFour === 'YES' || questionFour === 'Y') {
 } else {
   //console.log('You are correct! I am a Marine Corps veteran');
   alert('You are correct! I am a Marine Corps veteran');
+  score++;
 }
 
 let questionFive = prompt('Do I have 2 kids?').toUpperCase();
@@ -56,4 +61,51 @@ if (questionFive === 'YES' || questionFive === 'Y') {
 } else {
   //console.log('That is correct!');
   alert('That is correct!');
+  score++;
 }
+
+let attempts = 1;
+
+for (let i = 4; i >= attempts; i--) {
+  let correctAnswer = 8;
+  let numberGuesser = parseInt(prompt('Guess a number between 1-20'));
+
+  if (numberGuesser === correctAnswer) {
+    alert('You are correct.');
+    score++;
+    break;
+  } else if (numberGuesser < correctAnswer) {
+    alert(`Your guess is too low. You have ${i - 1} attempts remaining.`);
+  } else if (numberGuesser > correctAnswer) {
+    alert(`Your guess is too high. You have ${i} attempts remaining.`);
+  }
+
+  if (i === 1) {
+    alert(`The correct answer was ${correctAnswer}.`);
+    break;
+  }
+}
+let sports = ['baseball', 'hockey', 'basketball', 'football', 'wrestling'];
+
+//let userInput = prompt('What was a sport I played growing up?');
+
+let guesses = 0;
+let correct = false;
+for (let k = 6; k > guesses; k--) {
+  let abort = false;
+  let userInput = prompt('What was a sport I played growing up?');
+  for (let j = 0; j < sports.length; j++) {
+    if (userInput.toLowerCase() === sports[j]) {
+      alert('That is correct!');
+      correct = true;
+      score++;
+    }
+  }
+  if (!correct) {
+    alert(`You have ${[k - 1]} guesses left.`);
+  }
+  if (correct) break;
+}
+
+alert(`${sports}`);
+alert(`Your total score was ${score}`);
